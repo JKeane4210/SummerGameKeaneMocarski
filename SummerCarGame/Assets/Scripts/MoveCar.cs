226ea272f4682508f7b7a212809fe6d7abd453e2
JKeane4210 <64436387+JKeane4210@users.noreply.github.com>
2020-06-03 22:49:23 -0500

    Added Car Movement (need to make occur in local rotation's direction)

---
+++ SummerCarGame/Assets/Scripts/MoveCar.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCar : MonoBehaviour
+{
+    public float velocity;
+    public float angular_velocity;
+    public CharacterController car;
+    public Transform car_transform;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        if (Input.GetKey(KeyCode.W))
+        {
+            Vector3 move = transform.forward * z + transform.right* x;
+            car.Move(move * velocity * Time.deltaTime);
+            if (Input.GetKey(KeyCode.A))
+                car_transform.Rotate(Vector3.up * -angular_velocity);
+            if (Input.GetKey(KeyCode.D))
+                car_transform.Rotate(Vector3.up * angular_velocity);
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            Vector3 move = -1 * transform.forward * z - transform.right * x;
+            car.Move(move * -velocity * Time.deltaTime);
+            if (Input.GetKey(KeyCode.A))
+                car_transform.Rotate(Vector3.up * -angular_velocity);
+            if (Input.GetKey(KeyCode.D))
+                car_transform.Rotate(Vector3.up * angular_velocity);
+        }
+        
+    }
+}
