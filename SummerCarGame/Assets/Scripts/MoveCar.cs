8e6ffaece8749e08270b1ddb21eb7b273cf95e2b
JKeane4210 <64436387+JKeane4210@users.noreply.github.com>
2020-06-09 00:15:47 -0500

    Started changing movement script to turn more realistically

--- SummerCarGame/Assets/Scripts/MoveCar.cs
+++ SummerCarGame/Assets/Scripts/MoveCar.cs
@@ -8,10 +8,18 @@ public class MoveCar : MonoBehaviour
     public float angular_velocity;
     public CharacterController car;
     public Transform car_transform;
+    public Transform front_wheels;
+
+
+    private void Start()
+    {
+        front_wheels.Rotate(Vector3.up * (-0.1f)); // for whatever reason, the little offset allowed it to work
+    }
 
     // Update is called once per frame
     void Update()
     {
+        //
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
         if (Input.GetKey(KeyCode.W))
@@ -19,19 +27,42 @@ public class MoveCar : MonoBehaviour
             Vector3 move = transform.forward * z + transform.right* x;
             car.Move(move * velocity * Time.deltaTime);
             if (Input.GetKey(KeyCode.A))
-                car_transform.Rotate(Vector3.up * -angular_velocity);
+                wheelShift(true);
             if (Input.GetKey(KeyCode.D))
-                car_transform.Rotate(Vector3.up * angular_velocity);
+                wheelShift(false);
         }
         if(Input.GetKey(KeyCode.S))
         {
             Vector3 move = -1 * transform.forward * z - transform.right * x;
             car.Move(move * -velocity * Time.deltaTime);
             if (Input.GetKey(KeyCode.A))
-                car_transform.Rotate(Vector3.up * -angular_velocity);
+                wheelShift(true);
             if (Input.GetKey(KeyCode.D))
-                car_transform.Rotate(Vector3.up * angular_velocity);
+                wheelShift(false);
         }
-        
+        if (Input.GetKey(KeyCode.A))
+            wheelShift(true);
+        if (Input.GetKey(KeyCode.D))
+            wheelShift(false);
+        if ((front_wheels.rotation.eulerAngles.y < 360 && front_wheels.rotation.eulerAngles.y > 329) && !Input.GetKey(KeyCode.A))
+            front_wheels.Rotate(Vector3.up * (angular_velocity));
+        if ((front_wheels.rotation.eulerAngles.y > 0 && front_wheels.rotation.eulerAngles.y < 31) && !Input.GetKey(KeyCode.D))
+            front_wheels.Rotate(Vector3.up * (-angular_velocity));
+    }
+
+    void wheelShift(bool isRight)
+    {
+        if (isRight)
+        {
+            float maxAngle = 330;
+            if (front_wheels.rotation.eulerAngles.y > maxAngle)
+                front_wheels.Rotate(Vector3.up * -angular_velocity);
+        }
+        if (!isRight)
+        {
+            float maxAngle = 30;
+            if (front_wheels.rotation.eulerAngles.y < maxAngle)
+                front_wheels.Rotate(Vector3.up * angular_velocity);
+        }
     }
 }
