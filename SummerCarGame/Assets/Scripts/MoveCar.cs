e7fc584aab7ff9113e763cdf2145fc9b0cade6a5
JKeane4210 <64436387+JKeane4210@users.noreply.github.com>
2020-06-11 21:30:05 -0500

    Car movement

--- SummerCarGame/Assets/Scripts/MoveCar.cs
+++ SummerCarGame/Assets/Scripts/MoveCar.cs
@@ -4,65 +4,54 @@ using UnityEngine;
 
 public class MoveCar : MonoBehaviour
 {
-    public float velocity;
-    public float angular_velocity;
+    public float lateral_vel;
+    public float forward_vel;
     public CharacterController car;
     public Transform car_transform;
-    public Transform front_wheels;
-
-
-    private void Start()
-    {
-        front_wheels.Rotate(Vector3.up * (-0.1f)); // for whatever reason, the little offset allowed it to work
-    }
 
     // Update is called once per frame
     void Update()
     {
+        car_transform.localPosition = new Vector3(car_transform.localPosition.x, -1, car_transform.localPosition.z);
         //
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
         {
-            Vector3 move = transform.forward * z + transform.right* x;
-            car.Move(move * velocity * Time.deltaTime);
-            if (Input.GetKey(KeyCode.A))
-                wheelShift(true);
-            if (Input.GetKey(KeyCode.D))
-                wheelShift(false);
+            Vector3 move = transform.forward * z + transform.right * x;
+            car.Move(move * forward_vel * Time.deltaTime);
+            car_transform.localPosition = new Vector3(car_transform.localPosition.x, -1, car_transform.localPosition.z);
+            if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+            {
+                Vector3 move2 = transform.right * x + transform.forward * z;
+                car.Move(move2 * lateral_vel * Time.deltaTime);
+                car_transform.localPosition = new Vector3(car_transform.localPosition.x, -1, car_transform.localPosition.z);
+            }
+            if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
+            {
+                Vector3 move2 = -1 * transform.right * x - transform.forward * z;
+                car.Move(move2 * -lateral_vel * Time.deltaTime);
+                car_transform.localPosition = new Vector3(car_transform.localPosition.x, -1, car_transform.localPosition.z);
+            }
         }
-        if(Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.W))
         {
             Vector3 move = -1 * transform.forward * z - transform.right * x;
-            car.Move(move * -velocity * Time.deltaTime);
-            if (Input.GetKey(KeyCode.A))
-                wheelShift(true);
-            if (Input.GetKey(KeyCode.D))
-                wheelShift(false);
+            car.Move(move * -forward_vel * Time.deltaTime);
+            car_transform.localPosition = new Vector3(car_transform.localPosition.x, -1, car_transform.localPosition.z);
+            if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+            {
+                Vector3 move2 = transform.right * x + transform.forward * z;
+                car.Move(move2 * lateral_vel * Time.deltaTime);
+                car_transform.localPosition = new Vector3(car_transform.localPosition.x, -1, car_transform.localPosition.z);
+            }
+            if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
+            {
+                Vector3 move2 = -1 * transform.right * x - transform.forward * z;
+                car.Move(move2 * -lateral_vel * Time.deltaTime);
+                car_transform.localPosition = new Vector3(car_transform.localPosition.x, -1, car_transform.localPosition.z);
+            }
         }
-        if (Input.GetKey(KeyCode.A))
-            wheelShift(true);
-        if (Input.GetKey(KeyCode.D))
-            wheelShift(false);
-        if ((front_wheels.rotation.eulerAngles.y < 360 && front_wheels.rotation.eulerAngles.y > 329) && !Input.GetKey(KeyCode.A))
-            front_wheels.Rotate(Vector3.up * (angular_velocity));
-        if ((front_wheels.rotation.eulerAngles.y > 0 && front_wheels.rotation.eulerAngles.y < 31) && !Input.GetKey(KeyCode.D))
-            front_wheels.Rotate(Vector3.up * (-angular_velocity));
-    }
-
-    void wheelShift(bool isRight)
-    {
-        if (isRight)
-        {
-            float maxAngle = 330;
-            if (front_wheels.rotation.eulerAngles.y > maxAngle)
-                front_wheels.Rotate(Vector3.up * -angular_velocity);
-        }
-        if (!isRight)
-        {
-            float maxAngle = 30;
-            if (front_wheels.rotation.eulerAngles.y < maxAngle)
-                front_wheels.Rotate(Vector3.up * angular_velocity);
-        }
+        
     }
 }
---
+++ SummerCarGame/Assets/Scripts/MoveWheel.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWheel : MonoBehaviour
+{
+    public float velocity;
+    public float angular_velocity;
+    public CharacterController car;
+    public Transform car_transform;
+    public Transform front_wheels;
+
+
+    private void Start()
+    {
+        front_wheels.Rotate(Vector3.up * (-0.1f)); // for whatever reason, the little offset allowed it to work
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+            wheelShift(true);
+        if (Input.GetKey(KeyCode.D))
+            wheelShift(false);
+        if ((front_wheels.rotation.eulerAngles.y < 360 && front_wheels.rotation.eulerAngles.y > 329) && !Input.GetKey(KeyCode.A))
+            front_wheels.Rotate(Vector3.up * (angular_velocity));
+        if ((front_wheels.rotation.eulerAngles.y > 0 && front_wheels.rotation.eulerAngles.y < 31) && !Input.GetKey(KeyCode.D))
+            front_wheels.Rotate(Vector3.up * (-angular_velocity));
+    }
+
+    void wheelShift(bool isRight)
+    {
+        if (isRight)
+        {
+            float maxAngle = 330;
+            if (front_wheels.rotation.eulerAngles.y > maxAngle)
+                front_wheels.Rotate(Vector3.up * -angular_velocity);
+        }
+        if (!isRight)
+        {
+            float maxAngle = 30;
+            if (front_wheels.rotation.eulerAngles.y < maxAngle)
+                front_wheels.Rotate(Vector3.up * angular_velocity);
+        }
+    }
+}
