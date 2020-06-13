09697b93f96eaf5efd7059f8f4194042549b28fb
JKeane4210 <64436387+JKeane4210@users.noreply.github.com>
2020-06-13 14:20:29 -0500

    Deer running across screen

---
+++ SummerCarGame/Assets/Scripts/DeerRunning.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeerRunning : MonoBehaviour
+{
+    public float vel_min;
+    public float vel_max;
+    public Transform player;
+    public Rigidbody player_rigidbody;
+    public float motion_multiplier;
+    private Vector3 trajectory = Vector3.forward + Vector3.up * 1.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        trajectory = motion_multiplier * new Vector3(Mathf.Cos(player.localRotation.y), 0f, -Mathf.Sin(player.localRotation.y));
+    }
+
+    private void FixedUpdate()
+    {
+        player_rigidbody.velocity = trajectory;
+    }
+}
---
+++ SummerCarGame/Assets/Scripts/RandomDeer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomDeer : MonoBehaviour
+{
+    public GameObject deer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        deer.AddComponent<Rigidbody>();
+        deer.GetComponent<Rigidbody>().useGravity = false;
+        deer.AddComponent<DeerRunning>();
+        deer.GetComponent<DeerRunning>().
+    }
+
+}
