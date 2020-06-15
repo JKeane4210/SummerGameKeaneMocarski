f2b98a68d419db328e1fc8e6ed88a4dbdfd4c695
JKeane4210 <64436387+JKeane4210@users.noreply.github.com>
2020-06-15 08:14:13 -0500

    New branch, fixed scene

--- SummerCarGame/Assets/Scripts/SceneDrawing.cs
+++ SummerCarGame/Assets/Scripts/SceneDrawing.cs
@@ -19,14 +19,16 @@ public class SceneDrawing : MonoBehaviour
 
     private void Start()
     {
-        DrawRoadBlock(roadBlock);
-        DrawStaticRoadBlock(staticRoadBlock);
-        DrawCar(car);
+        //DrawRoadBlock(roadBlock);
+        //DrawStaticRoadBlock(staticRoadBlock);
+        //DrawCar(car);
+        ConnectCarToBars(car);
         ResetHealthBar(healthBar);
         ResetFuelBar(fuelBar);
         HideButton(replay_btn);
         HideButton(home_btn);
         HideButton(game_over_txt_fied);
+        
     }
 
     public void DrawRoadBlock(GameObject road_block)
@@ -39,6 +41,14 @@ public class SceneDrawing : MonoBehaviour
     public void DrawStaticRoadBlock(GameObject static_road_block)
     {
         Instantiate(static_road_block, new Vector3(0f, 0f, 0f), Quaternion.Euler(0f, 0f, 0f));
+    }
+
+    public void ConnectCarToBars(GameObject car)
+    {
+        //car.GetComponent<Car>().fuelBar = fuel_bar_obj.GetComponent<FuelBar>();
+        //car.GetComponent<Car>().healthBar = health_bar_obj.GetComponent<HealthBar>();
+        car.GetComponent<CarDeerCollide>().health_bar = health_bar_obj;
+        Debug.Log("Completed");
     }
 
     public void DrawCar(GameObject car)
