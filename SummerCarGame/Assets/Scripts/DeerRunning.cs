605c338a36003b2a695cc53ffa4ccc04b0b4d154
JKeane4210 <64436387+JKeane4210@users.noreply.github.com>
2020-06-21 12:37:10 -0500

    Deer running from both sides of road

--- SummerCarGame/Assets/Scripts/DeerRunning.cs
+++ SummerCarGame/Assets/Scripts/DeerRunning.cs
@@ -12,7 +12,14 @@ public class DeerRunning : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        trajectory = motion_multiplier * new Vector3(Mathf.Cos(player.localRotation.y), 0f, -Mathf.Sin(player.localRotation.y));
+        float degreeRotY = player.localRotation.eulerAngles.y;
+        print(degreeRotY);
+        //trajectory = motion_multiplier * new Vector3(Mathf.Cos(player.localRotation.eulerAngles.y), 0f, Mathf.Sin(player.localRotation.eulerAngles.y));
+
+        if (degreeRotY <= 151f && degreeRotY >= 89f)
+            trajectory = motion_multiplier * new Vector3(Mathf.Cos(player.localRotation.y), 0f, -Mathf.Sin(player.localRotation.y));
+        else if (degreeRotY <= 271f && degreeRotY >= 209f)
+            trajectory = motion_multiplier * new Vector3(-Mathf.Cos(player.localRotation.y), 0f, -Mathf.Sin(player.localRotation.y));
     }
 
     private void FixedUpdate()
