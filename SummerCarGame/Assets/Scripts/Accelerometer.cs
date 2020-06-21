00178ae0e19f1f7131e50567b81585f6738289b2
kacpermoca <65927572+kacpermoca@users.noreply.github.com>
2020-06-21 16:49:36 -0500

    Tilt

---
+++ SummerCarGame/Assets/Scripts/Accelerometer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Accelerometer : MonoBehaviour
+{
+
+    private Rigidbody rigid;
+    public float speed;
+    private void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(Input.acceleration.x, 0f, 0f);
+        rigid.velocity = movement * speed;
+    }
+
+    private void Update()
+    {
+       
+    }
+ 
+}
--- SummerCarGame/Assets/Scripts/SwipeTest.cs
+++ SummerCarGame/Assets/Scripts/SwipeTest.cs
@@ -9,21 +9,26 @@ public class SwipeTest : MonoBehaviour
     public Transform player;
     private Vector3 desiredPosition;
     public float swipeVelocity;
+    public float speed = 10f;
 
+   
     private void Start()
     {
         desiredPosition = player.transform.position;  
+
     }
     private void Update()
     {
         if(swipeControls.SwipeLeft)
-            desiredPosition += Vector3.left * swipeVelocity;
+            desiredPosition += Vector3.left * swipeVelocity * speed * Time.deltaTime;
         if(swipeControls.SwipeRight)
-            desiredPosition += Vector3.right * swipeVelocity;
+            desiredPosition += Vector3.right * swipeVelocity * speed * Time.deltaTime;
 
-        player.transform.position = Vector3.MoveTowards(player.transform.position, desiredPosition, 3f * Time.deltaTime);
-        
+        player.transform.position = Vector3.MoveTowards(player.transform.position, desiredPosition, 10f * Time.deltaTime);
+
         if(swipeControls.Tap)
             Debug.Log("Tap");
+        
     }
+    
 }
