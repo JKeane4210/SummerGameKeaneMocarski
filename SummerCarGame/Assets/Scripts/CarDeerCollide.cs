ac412420d254aaf6f784c8da86079ba48bf7c6e3
JKeane4210 <64436387+JKeane4210@users.noreply.github.com>
2020-06-24 19:20:14 -0500

    Settings to enable explosions

--- SummerCarGame/Assets/Scripts/CarDeerCollide.cs
+++ SummerCarGame/Assets/Scripts/CarDeerCollide.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 
 public class CarDeerCollide : MonoBehaviour
 {
+    static bool explosionsEnabled = false;
     public GameObject health_bar;
     private float health_lost = 10f;
-    bool hasExploded = false;
     public GameObject explosionEffect;
     public DeerRunning deer;
     
-
     private void OnTriggerEnter(Collider other)
     {
         //Debug.Log(other);
@@ -20,14 +19,33 @@ public class CarDeerCollide : MonoBehaviour
             //Debug.Log(other);
             health.DecreaseHealth(health_lost);
             Explode();
-            
         }
     }
 
     void Explode()
     {
-        Instantiate(explosionEffect, transform.position, transform.rotation);
+        if(explosionsEnabled)
+            Instantiate(explosionEffect, transform.position, transform.rotation);
         DestroyImmediate(deer.player, true);
+    }
+
+    public void ChangeExplosionsStatus()
+    {
+        if(explosionsEnabled)
+        {
+            explosionsEnabled = false;
+            gameObject.GetComponent<UnityEngine.UI.Text>().text = "Explosions Enabled: No";
+        }
+        else if(!explosionsEnabled)
+        {
+            explosionsEnabled = true;
+            gameObject.GetComponent<UnityEngine.UI.Text>().text = "Explosions Enabled: Yes";
+        }
+    }
+
+    public bool GetExplosionsStatus()
+    {
+        return explosionsEnabled;
     }
 }
 
