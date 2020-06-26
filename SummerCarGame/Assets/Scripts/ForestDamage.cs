1e80ad99261bac76bf19d722c9ab93a9f7971009
JKeane4210 <64436387+JKeane4210@users.noreply.github.com>
2020-06-25 23:39:07 -0500

    forest damage fixed
    can now veer off for gas without taking damage

--- SummerCarGame/Assets/Scripts/ForestDamage.cs
+++ SummerCarGame/Assets/Scripts/ForestDamage.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class ForestDamage : MonoBehaviour
 {
     public GameObject car;
-    public float limit;
+    public float leftLimit;
+    public float rightLimit;
     public GameObject health;
     private HealthBar healthBar;
     public float normalSpeed;
@@ -19,9 +20,9 @@ public class ForestDamage : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if(Mathf.Abs(car.transform.position.x) > limit)
+        if(car.transform.position.x < leftLimit)
         {
-            float damage = Mathf.Abs(car.transform.position.x) - limit + 1;
+            float damage = Mathf.Abs(car.transform.position.x - leftLimit) + 1;
             healthBar.DecreaseHealth(damage*damage);
             if (car.GetComponent<SwipeControls>().velocity > 10)
             {
@@ -30,11 +31,41 @@ public class ForestDamage : MonoBehaviour
                 car.GetComponent<Accelerometer>().forward_vel -= deltaV;
             }  
         }
+        else if(car.transform.position.x > rightLimit)
+        {
+            float damage = Mathf.Abs(car.transform.position.x - rightLimit) + 1;
+            healthBar.DecreaseHealth(damage * damage);
+            if (car.GetComponent<SwipeControls>().velocity > 10)
+            {
+                car.GetComponent<SwipeControls>().velocity -= deltaV;
+                car.GetComponent<MoveCar>().forward_vel -= deltaV;
+                car.GetComponent<Accelerometer>().forward_vel -= deltaV;
+            }
+        }
         else
         {
             car.GetComponent<SwipeControls>().velocity = normalSpeed;
             car.GetComponent<MoveCar>().forward_vel = normalSpeed;
             car.GetComponent<Accelerometer>().forward_vel = normalSpeed;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.ToString().Contains("forestRoad"))
+        {
+            if((int)(other.transform.eulerAngles.y / 180) % 2 == 1)
+            {
+                print("exception triggered!");
+                leftLimit = -other.GetComponent<RoadSides>().rightSide;
+                rightLimit = -other.GetComponent<RoadSides>().leftSide;
+            }
+            else
+            {
+                leftLimit = other.GetComponent<RoadSides>().leftSide;
+                rightLimit = other.GetComponent<RoadSides>().rightSide;
+            }
+            
         }
     }
 }
---
+++ SummerCarGame/Assets/Scripts/RoadSides.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadSides : MonoBehaviour
+{
+    public float leftSide = 0;
+    public float rightSide = 0;
+}
