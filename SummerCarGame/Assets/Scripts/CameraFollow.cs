4afc9f2fde0bf96619409289cbe4a6b9f895bf7e
JKeane4210 <64436387+JKeane4210@users.noreply.github.com>
2020-06-27 16:33:55 -0500

    Changed Up Camera Following

---
+++ SummerCarGame/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private GameObject car;
+    // Start is called before the first frame update
+    void Start()
+    {
+        car = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(car.transform.position.x / 1.75f, 13f, car.transform.position.z - 5f);
+    }
+}
