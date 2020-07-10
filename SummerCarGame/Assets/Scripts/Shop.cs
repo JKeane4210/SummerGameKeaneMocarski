73943e3eba7efc661103b9652c9420645b3a0842
kacpermoca <65927572+kacpermoca@users.noreply.github.com>
2020-07-10 17:25:57 -0500

    AutoShop/Store Progress

---
+++ SummerCarGame/Assets/Scripts/Shop.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shop : MonoBehaviour
+{
+   GameObject ItemTemplate;
+   GameObject g;
+   [SerializeField] Transform ShopScrollView;
+
+   void Start()
+   {
+       ItemTemplate = ShopScrollView.GetChild(0).gameObject;
+
+       for (int i = 0; i < 12; i++)
+       {
+           g = Instantiate (ItemTemplate, ShopScrollView);
+       }
+
+       Destroy(ItemTemplate);
+   }
+}
