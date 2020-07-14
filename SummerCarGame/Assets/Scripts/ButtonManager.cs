6c88aeb59a6cf60a37428e4cf458edb08565e9a1
JKeane4210 <64436387+JKeane4210@users.noreply.github.com>
2020-07-14 01:13:45 -0500

    Loading screen
    Will try to improve with animation of rotation

--- SummerCarGame/Assets/Scripts/ButtonManager.cs
+++ SummerCarGame/Assets/Scripts/ButtonManager.cs
@@ -2,13 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ButtonManager : MonoBehaviour
 {
-    public string level;
+    public GameObject loadingScreen;
+    public GameObject fuelNeedle;
+
+    void Start()
+    {
+        if(loadingScreen != null)
+            loadingScreen.SetActive(false);
+    }
 
     public void ButtonChangeScene(string level)
     {
-        SceneManager.LoadScene(level);
+        StartCoroutine(LoadAsynchronously(level));
+    }
+
+    IEnumerator LoadAsynchronously(string level)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(level);
+        if(loadingScreen != null)
+            loadingScreen.SetActive(true);
+        while(!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            Debug.Log(progress);
+            if(fuelNeedle != null)
+                fuelNeedle.GetComponent<RectTransform>().rotation = Quaternion.Euler(0, 0, 90f + 180f * progress);
+            //System.Threading.Thread.Sleep(2000);
+            yield return null;
+        }
     }
 }
