c4048f280a4a1307d1e3b1c2e58bcecb3408e0f0
JKeane4210 <64436387+JKeane4210@users.noreply.github.com>
2020-07-17 15:13:14 -0500

    car unlocked screen

---
+++ SummerCarGame/Assets/Scripts/UnlockedPage_.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UnlockedPage_ : MonoBehaviour
+{
+    public GameObject unlockedPagePanel;
+    public GameObject nameText;
+    public GameObject nameDescrText;
+    public GameObject okButton;
+    private GameObject car;
+
+    void Start()
+    {
+        unlockedPagePanel.SetActive(false);
+        okButton.GetComponent<Button>().onClick.AddListener(delegate { unlockedPagePanel.SetActive(false); });
+        okButton.GetComponent<Button>().onClick.AddListener(delegate { DestroyCar(); });
+    }
+
+    public void DestroyCar()
+    {
+        car = GameObject.FindGameObjectWithTag("Player");
+        Destroy(car);
+    }
+}
