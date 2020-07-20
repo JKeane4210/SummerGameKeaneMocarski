ab4c6dc0ab8fb49c5c0752ecf00403dfcd511b60
JKeane4210 <64436387+JKeane4210@users.noreply.github.com>
2020-07-19 23:22:09 -0500

    Day mode time slider

---
+++ SummerCarGame/Assets/Scripts/SunSlider.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunSlider : MonoBehaviour
+{
+    public GameObject background;
+    public GameObject fill;
+}
