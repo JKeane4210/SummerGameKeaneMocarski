65d3d263bc7a3c6012d6c872328f89d6e3327022
JKeane4210 <64436387+JKeane4210@users.noreply.github.com>
2020-07-24 00:19:41 -0500

    Parallax tweaked

--- SummerCarGame/Assets/Scripts/Parallax.cs
+++ SummerCarGame/Assets/Scripts/Parallax.cs
@@ -7,22 +7,40 @@ public class Parallax : MonoBehaviour
     private float length, startpos;
     public GameObject cam;
     public float parallaxEffect;
+
     void Start()
     {
+        Time.timeScale = 1;
         startpos = transform.position.x;
         length = GetComponent<SpriteRenderer>().bounds.size.x;
         
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        float temp = (cam.transform.position.x * (1 - parallaxEffect));
-        float distance = (cam.transform.position.x * parallaxEffect);
-        transform.position = new Vector3(startpos + distance, transform.position.y, transform.position.z);
-        
-        if (temp > startpos + length) 
-            startpos += length;
-        else if (temp < startpos - length)
-             startpos -= length;
+        if (parallaxEffect != 0)
+        {
+
+
+            transform.position = new Vector3(transform.position.x - 2 * (1 - parallaxEffect) * Time.deltaTime, transform.position.y, transform.position.z);
+            //float temp = (transform.position.x * (1 - parallaxEffect)); // >>> 0
+            //float distance = (transform.position.x * parallaxEffect); // >>> 0
+            //print(distance);
+
+            //transform.position = new Vector3(startpos + distance, transform.position.y, transform.position.z);
+
+            if (transform.position.x <= -10)
+                transform.position = new Vector3(transform.position.x + length, transform.position.y, transform.position.z);
+
+            //if (distance > startpos + length)
+            //    startpos += length;
+            //if (distance < startpos - length)
+            //    startpos -= length;
+        }
+        else
+        {
+            transform.position = new Vector3(startpos + 1 * Mathf.Sin(Time.timeSinceLevelLoad), transform.position.y, transform.position.z);
+
+        }
     }
 }
