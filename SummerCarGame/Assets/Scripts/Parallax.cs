19a5cca9a42567706a36321e662868085d140db1
kacpermoca <65927572+kacpermoca@users.noreply.github.com>
2020-07-23 23:28:50 -0500

    parallax pt2

--- SummerCarGame/Assets/Scripts/Parallax.cs
+++ SummerCarGame/Assets/Scripts/Parallax.cs
@@ -11,17 +11,18 @@ public class Parallax : MonoBehaviour
     {
         startpos = transform.position.x;
         length = GetComponent<SpriteRenderer>().bounds.size.x;
+        
     }
 
-    
-    void FixedUpdate()
+    void Update()
     {
         float temp = (cam.transform.position.x * (1 - parallaxEffect));
-        float distance = (cam.transform.position
-        .x * parallaxEffect);
+        float distance = (cam.transform.position.x * parallaxEffect);
         transform.position = new Vector3(startpos + distance, transform.position.y, transform.position.z);
-   
-        if (temp > startpos + length) startpos += length;
-        else if (temp < startpos + length) startpos -= length;
+        
+        if (temp > startpos + length) 
+            startpos += length;
+        else if (temp < startpos - length)
+             startpos -= length;
     }
 }
