85719189d1b224e0898e6f2a31a67209df15d2f5
JKeane4210 <64436387+JKeane4210@users.noreply.github.com>
2020-07-26 18:43:17 -0500

    Savannah Prefabs, WorldTerrain Class

---
+++ SummerCarGame/Assets/Scripts/WorldTerrain.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldTerrain
+{
+    private string name;
+    private GameObject normalRoad;
+    private GameObject gasRoad;
+    private GameObject[] animals;
+
+    public WorldTerrain(string n, GameObject road, GameObject gas, GameObject[] anims)
+    {
+        name = n;
+        normalRoad = road;
+        gasRoad = gas;
+        animals = anims;
+    }
+
+    public string GetName()
+    {
+        return name;
+    }
+
+    public GameObject GetNormalRoad()
+    {
+        return normalRoad;
+    }
+
+    public GameObject GetGasRoad()
+    {
+        return gasRoad;
+    }
+
+    public GameObject[] GetAnimals()
+    {
+        return animals;
+    }
+}
---
+++ SummerCarGame/Assets/Scripts/WorldTerrainList.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldTerrainList : MonoBehaviour
+{
+    static WorldTerrain selectedTerrain;
+    private WorldTerrain[] worldTerrains = new WorldTerrain[]
+        {
+            new WorldTerrain("North Woods",
+                             (GameObject)Resources.Load("Models/Roads/forestRoadNormal"),
+                             (GameObject)Resources.Load("Models/Roads/forestRoadGasStopPainted"),
+                             new GameObject[]{
+                                 (GameObject)Resources.Load("Models/Animals/deer3")
+                             }),
+            new WorldTerrain("Savannah",
+                            (GameObject)Resources.Load("Models/Roads/Savannah/savannahRoadNormal.prefab"),
+                            (GameObject)Resources.Load("Models/Roads/Savannah/savannahRoadGasStop.prefab"),
+                            new GameObject[]{
+                                            }
+                            )
+        };
+
+    void Start()
+    {
+        if (selectedTerrain == null)
+            selectedTerrain = worldTerrains[0];
+    }
+
+    public WorldTerrain GetSelectedTerrain()
+    {
+        return selectedTerrain;
+    }
+
+    public GameObject GetStaticNormalRoad()
+    {
+        return selectedTerrain.GetNormalRoad();
+    }
+
+    public GameObject GetActiveRoad()
+    {
+        return selectedTerrain.GetNormalRoad();
+    }
+
+    public GameObject GetGasRoad()
+    {
+        return selectedTerrain.GetGasRoad();
+    }
+}
