cb80a3a61b57e9affc4946323a96551ee530f630
kacpermoca <65927572+kacpermoca@users.noreply.github.com>
2020-08-10 23:00:17 -0500

    nothing imp

--- SummerCarGame/Assets/Scripts/MainMenuCar.cs
+++ SummerCarGame/Assets/Scripts/MainMenuCar.cs
@@ -15,26 +15,6 @@ public class MainMenuCar : MonoBehaviour
         car.transform.localScale = new Vector3(vehicle.mainMenuScaleX, vehicle.mainMenuScaleY, vehicle.mainMenuScaleZ);
         car.transform.rotation = vehicle.mainMenuRotation;
         
-        //Default Car
-        // car.transform.localScale = new Vector3(vehicle.GetViewingScale().x/7, vehicle.GetViewingScale().y/7, vehicle.GetViewingScale().z /7);
-        // car.transform.position = new Vector3(vehicle.GetViewingLocation().x - 4, vehicle.GetViewingLocation().y + 24, vehicle.GetViewingLocation().z + 150);
-        // car.transform.rotation = Quaternion.Euler(6, 145, 0);
-
-        //SandVan
-        // car.transform.localScale = new Vector3(vehicle.GetViewingScale().x/12, vehicle.GetViewingScale().y/12, vehicle.GetViewingScale().z /12);
-        // car.transform.position = new Vector3(vehicle.GetViewingLocation().x - 4, vehicle.GetViewingLocation().y + 18, vehicle.GetViewingLocation().z + 150);
-        // car.transform.rotation = Quaternion.Euler(5, 145, 0);
-
-        //Mr Conroy
-        // car.transform.localScale = new Vector3(vehicle.GetViewingScale().x/12, vehicle.GetViewingScale().y/12, vehicle.GetViewingScale().z /12);
-        // car.transform.position = new Vector3(vehicle.GetViewingLocation().x - 4, vehicle.GetViewingLocation().y + 15, vehicle.GetViewingLocation().z + 150);
-        // car.transform.rotation = Quaternion.Euler(5, 145, 0);
-
-        //Night Cruiser
-        // car.transform.localScale = new Vector3(vehicle.GetViewingScale().x/12, vehicle.GetViewingScale().y/12, vehicle.GetViewingScale().z /12);
-        // car.transform.position = new Vector3(vehicle.GetViewingLocation().x - 4, vehicle.GetViewingLocation().y + 7, vehicle.GetViewingLocation().z + 150);
-        // car.transform.rotation = Quaternion.Euler(5, 145, 0);
-
     }
 
 
